e24d62341b6e92ac1003f750d6e365d1a3223b2a
parin <team@yoloperks.com>
2019-09-14 14:06:36 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,17 +14,15 @@ weatherForm.addEventListener("submit", e => {
   if (!location) {
     messageOne.textContent = "Please enter an address";
   } else {
-    fetch(`http://localhost:3000/weather?address=${location}`).then(
-      response => {
-        response.json().then(data => {
-          if (data.error) {
-            messageOne.textContent = data.error;
-          } else {
-            messageOne.textContent = data.location;
-            messageTwo.textContent = data.forecast;
-          }
-        });
-      }
-    );
+    fetch(`/weather?address=${location}`).then(response => {
+      response.json().then(data => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    });
   }
 });
